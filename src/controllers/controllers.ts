1a3201816527f3ec5798be36aa067ce3d9a0bad4
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-06-21 15:52:26 +0100

    arrows

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,30 +1,27 @@
 import { Request, Response } from "express";
 
-
-function getLogin(req: Request, res: Response): void {
+const getLogin = (req: Request, res: Response): void => {
     res.render("login");
-}
+};
 
-function postLogin(req: Request, res: Response): void {
+const postLogin = (req: Request, res: Response): void => {
     let username = req.body.username;
     let password = req.body.password;
 
     if (username === "admin" && password === "letmein") {
         console.log("login success");
-
-        // req.session.login = true; // Set session value
-        req.session.login = true; // Type assertion to any
+        (req.session as any).login = true; // Type assertion to any
         console.log(req.session);
         res.redirect("/secureplace");
     } else {
         res.redirect("/login");
     }
-}
+};
 
-function logout(req: Request, res: Response): void {
+const logout = (req: Request, res: Response): void => {
     req.session.destroy(function (err) {
         res.redirect("/login");
     });
-}
+};
 
 export { getLogin, postLogin, logout };
